c3e6a6ba18c6c4d11461690642ca96a83e20fdc4
根津泰之 <nezuyasuyuki@nedzuyasuyukinoMacBook-puro.local>
2019-11-29 11:29:00 +0900

    フォームの送信ボタンを押下後、更新するように変更

--- js/test.js
+++ js/test.js
@@ -3,9 +3,9 @@ var count = 3;
 var game = "switch";
 var i =  Math.floor(Math.random() * 10);
 var message = new Array(
-  '今日は雨が降るでしょう', '今日は晴れるでしょう', '今日は曇りでしょう',
-  '今日は雨が降るでしょう', '今日は晴れるでしょう', '今日は曇りでしょう',
-  '今日は雪でしょう',
+  'オヤジのカミナリが落ちるでしょう', 'ジジイのカミナリが落ちるでしょう', 'おかんの怒りが爆発するでしょう',
+  'オヤジギャグが吹き荒れるでしょう', '私の心は大嵐でしょう', 'オヤジギャグが吹き荒れるでしょう',
+  '私の心は大嵐でしょう',
    'オヤジのカミナリが落ちるでしょう', 'オヤジギャグが吹き荒れるでしょう', 'おかんの怒りが爆発するでしょう');
    var greet = new Array(
     'あけましておめでとう', 'こたつのオトモに', '別れの季節ですね',
@@ -13,10 +13,10 @@ var message = new Array(
     '最近、暑くて倒れそう',　'夏休みだ！', '運動会だ！',
     'そろそろ、紅葉しているだろうか', '最近、寒くね？', '今年も終わりですね');
 
-document.write("<br>天気予報　" + message[i]);
+document.write("<br>タメにならない天気予報　" + message[i]);
 
 
 var day = new Date();
 document.write("<br><br>" + (day.getMonth()+1) + "月です。"　+ "<br>"  );
 document.write(greet[day.getMonth()] + "<br>"  );
-document.write("<br>" + day.getHours() + "時をとっくに過ぎています。"  );
+document.write("<br>" + day.getHours() + "時をとっくに過ぎています。<br>早く仕事しましょう。"  );
